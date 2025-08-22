a9ecc05c51f8b4e7bf80ec10998fb809f3c62c5f
Gyanendra Mohan Patel <gmpatel152@gmail.com>
2025-08-22 23:09:35 +0530

    linear search

---
+++ LinearSearch.java
@@ -0,0 +1,21 @@
+public class IterativeBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50 };
+        // int[] arr = {};
+        int element = 1;
+        int indexOfSearchedElement = search(arr, element);
+        System.out.println(indexOfSearchedElement);
+    }
+
+    public static int search(int[] arr, int k) {
+        if (arr.length <= 0)
+            return -1;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == k) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
