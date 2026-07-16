e5ed7dffcffcbc689bd82470603c22ec8316a7b9
Gyanendra Mohan Patel <gmpatel152@gmail.com>
2026-07-16 15:30:58 +0530

    java Arrays and sort method

--- Arrays/Array_prac/Main.java
+++ Arrays/Array_prac/Main.java
@@ -1,6 +1,22 @@
 package Arrays.Array_prac;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
-    int[] myArr = new int[5];
-    
+    public static void main(String[] args) {
+        int[] firstArray = getRandomArray(10);
+        System.out.println("First Array: "  + Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println("Sorted First Array: " + Arrays.toString(firstArray));
+    }
+
+    private static int[] getRandomArray(int len) {
+        Random random = new Random();
+        int[] newint = new int[len];
+        for(int i = 0; i < len; i++) {
+            newint[i] = random.nextInt(100);
+        }   
+        return newint;
+    }
 }
