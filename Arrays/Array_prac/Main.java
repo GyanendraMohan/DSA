8b6c85f1058c000f87f84d4f4641acc183f29e7d
Gyanendra Mohan Patel <gmpatel152@gmail.com>
2026-07-16 15:33:47 +0530

    arrays fill method

--- Arrays/Array_prac/Main.java
+++ Arrays/Array_prac/Main.java
@@ -2,13 +2,17 @@ package Arrays.Array_prac;
 
 import java.util.Random;
 import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int[] firstArray = getRandomArray(10);
         System.out.println("First Array: "  + Arrays.toString(firstArray));
         Arrays.sort(firstArray);
         System.out.println("Sorted First Array: " + Arrays.toString(firstArray));
+
+        int[] secondArray = getRandomArray(10);
+        System.out.println("Second Array: " + Arrays.toString(secondArray));
+        Arrays.fill(secondArray, 5);
+        System.out.println("Filled Second Array: " + Arrays.toString(secondArray));
     }
 
     private static int[] getRandomArray(int len) {
